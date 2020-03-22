befa7708b2a7cf19f7add0763452d4a30f391813
fkoyo <fu.ko.0428@gmail.com>
2020-03-22 13:44:41 +0900

    トップページの作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   devise_for :users
-  root to: 'posts#index'
+  root to: 'top#index'
   resources :posts, only: [:index, :new, :create, :show] do
     resources :comments, only: :create
   end
