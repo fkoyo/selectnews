7891bdfd1e64bbc7c9c37627a49ad12a28fb4832
fkoyo <fu.ko.0428@gmail.com>
2020-03-28 12:57:45 +0900

    likesのcreateアクションの実装

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,4 +2,14 @@ class LikesController < ApplicationController
   def new
     @like = Like.new
   end
+
+  def create
+    Like.create(like_params)
+    redirect_to posts_path
+  end
+
+  private
+  def like_params
+    params.require(:like).permit(:like_name)
+  end
 end
